5ae10bd486acb6024bbed58cad972204585e3c5b
tomimartin95 <tomimartin95@gmail.com>
2022-10-17 12:21:31 -0300

    Visualizar objeto en html

--- main.js
+++ main.js
@@ -10,10 +10,11 @@ class Animal {
         this.visitaVete = visitaVete;
         this.vacunas = vacunas;
     }
-}
+};
 
 //En consola
 let china = new Animal("China", "Gisela Flores", "Romano", 2, "dormir en su silloncito", "Wiskas", { Día: "28", Mes: "septiembre", Horario: "15:00 hs" }, { Vacuna_A: true, Vacuna_B: true, Vacuna_C: false });
 console.log(china);
 
 //En el HTML
+document.querySelector("ul").innerHTML = `Nombre: ${china.nombre} </br> Dueña: ${china.dueña} </br> Raza: ${china.raza} </br> Edad: ${china.edad} </br> Actividades: ${china.actividades} </br> Comida: ${china.comida} </br> Visita al veterinario: ${china.veterinario} </br> Vacunas: ${china.vacunas}`;
